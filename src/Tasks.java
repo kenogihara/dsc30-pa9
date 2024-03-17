4843333394e73e9ea78354ebb5e71bb018c4d9be
kenogihara <kenogihara808@gmail.com>
2024-03-17 11:31:42 -0700

    bruh

--- src/Tasks.java
+++ src/Tasks.java
@@ -1,26 +1,34 @@
 /*
- * Name: TODO
- * PID: TODO
+ * Name: Ken Ogihara
+ * PID: A16969236
  */
 
 import java.util.*;
 
 /**
- * TODO
+ * Tasks Implementation.
  *
- * @author TODO
- * @since TODO
+ * @author Ken Ogihara
+ * @since ${3/17/24}
  */
 
 public class Tasks {
 
     /**
-     * TODO: Question A
-     * @param entries
-     * @return
+     * Method that checks for any duplicates in an array of integers.
+     * @param entries an array of integers representing customer ID's.
+     * @return whether or not there are duplicates.
      */
     public static boolean raffleChecker(int[] entries) {
-        // TODO
+        LinkedList<Integer> output = new LinkedList<>();
+        for (int i = 0; i < entries.length; i++) {
+            if (!output.contains(entries[i])) {
+                (output).add(entries[i]);
+            }
+            else {
+                return true;
+            }
+        }
         return false;
     }
 
@@ -34,35 +42,45 @@ public class Tasks {
     }
 
     /**
-     * TODO: Question B
-     * @param head
-     * @return
+     * Method that finds the middle element of a Singly Linked List.
+     * @param head node
+     * @return string that represents the key of the middle node.
      */
     public static String middleNode(Node head) {
-        // TODO
-        return null;
+        if (head.next == null) {
+            throw new NullPointerException("list is empty");
+        }
+        else {
+            Node slow = head;
+            Node fast = head;
+            while (fast != null && fast.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return slow.name;
+        }
     }
 
     /**
-     * TODO: Question C
-     * @param newMessage
-     * @param oldMessage
-     * @return
+     * Method that checks if a new string can be printed with just the letters from an old string.
+     *
+     * @param newMessage a string.
+     * @param oldMessage a string.
+     * @return whether or not the new string can be constructed with just the letters of the old
+     * string.
      */
     public static boolean canConstruct(String newMessage, String oldMessage) {
-       // TODO
-        return false;
+
     }
 
-    /**TODO:
+    /** Method that returns a list of the most loyal customers given an integer k.
      * Question D
-     * @param buyers
-     * @param k
-     * @return
+     * @param buyers an array of strings of people who bought tickets in the last month.
+     * @param k an integer.
+     * @return an array of strings that contains the most loyal customers.
      */
     public static String[] frequentCustomers(String[] buyers, int k) {
-        // TODO
-        return null;
+
     }
 
 
---
+++ src/TasksTest.java
@@ -0,0 +1,9 @@
+class Tester {
+    public static void main(String args[]) {
+        System.out.println(Tasks.raffleChecker(new int[]{123, 456, 789}));
+        System.out.println(Tasks.raffleChecker(new int[]{123, 456, 789, 123, 999, 888}));
+        System.out.println(Tasks.raffleChecker(new int[]{2, 22, 3}));
+
+        System.out.println("");
+    }
+}
